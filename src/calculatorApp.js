91b2dd76ea36bdf6c34887a835b2d046138a9eeb
Roxc2301 <131661824+Roxc2301@users.noreply.github.com>
2023-06-11 21:23:04 +0200

    0.2
    The design seems to be done, but now the JS part is needed

---
+++ src/calculatorApp.js
@@ -0,0 +1,24 @@
+/*
+Year has to be 4 digits
+Month has to be 2 digits
+Day has to be 2 digits
+
+So first i have to get how many days i have.
+
+So 1 month equals 30 days,
+and a year is 365 days.
+
+
+*/
+function ageCalculator(day, month, year){
+    /* get the sum of days */
+    const daysYear = 365;
+    const daysMonth = 30;
+    const date = new Date().getFullYear();
+    const yearValue= date - year;
+    let sum = 0;
+    sum = day + (month*daysMonth) + (yearValue*daysYear);
+    return sum/365;
+}
+
+console.log(ageCalculator(24, 09, 1984));
